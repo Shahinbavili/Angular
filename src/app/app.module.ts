bda9f8f566628a6bb388cdddd9d5dded62e24c9a
shahin.bavili <shahin.bavili@useweb.com>
2023-09-18 09:48:43 +0200

    configure local to fr-FR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
-import {NgOptimizedImage} from "@angular/common";
+import {NgOptimizedImage, registerLocaleData} from "@angular/common";
+
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,13 @@ import {NgOptimizedImage} from "@angular/common";
         BrowserModule,
         NgOptimizedImage
     ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
