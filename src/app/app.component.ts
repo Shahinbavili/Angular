2cd5eba458216e952ce3caa1a4b18e56a64bf38f
shahin.bavili <shahin.bavili@useweb.com>
2023-09-20 15:35:36 +0200

    use map operator pipe function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {interval, Observable} from "rxjs";
+import {interval, map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      map(value => value % 2 === 0 ?
+        `Je suis ${ value } et je suis pair` :
+        `Je suis ${ value } et je suis impair`
+      )
+    );
   };
+
 }
+
