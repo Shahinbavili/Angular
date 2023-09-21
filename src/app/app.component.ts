38293366a99cafcf3ca673d08bd24611c02f5e54
shahin.bavili <shahin.bavili@useweb.com>
2023-09-21 18:05:29 +0200

    switchMap higher-order Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {concatMap, delay, exhaustMap, interval, map, mergeMap, of, take, tap} from "rxjs";
+import {concatMap, delay, exhaustMap, interval, map, mergeMap, of, switchMap, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
       map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
       tap(color => console.log(`La lumière s'allume en %c${ color }`, `color:
       ${ this.translateColor(color) }`)),
-      exhaustMap(color => this.getTrainObservable$(color)),
+      switchMap(color => this.getTrainObservable$(color)),
       tap(train => console.log(`Train %c${ train.color } ${ train.trainIndex } arrivé !`, `font-weight:
       bold; color: ${ this.translateColor(train.color) }`))
     ).subscribe();
