5aadbe60cbc69212b53120bf2197bf5fe9905856
shahin.bavili <shahin.bavili@useweb.com>
2023-09-25 10:49:48 +0200

    use OnDestroy life cycle hook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FaceSnap} from "./models/face-snap.model";
-import {FaceSnapsService} from "./services/face-snaps.service";
-import {interval, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -11,17 +8,10 @@ import {interval, take, tap} from "rxjs";
 
 export class AppComponent implements OnInit {
 
-  faceSnaps!: FaceSnap[];
-
-  constructor(private faceSnapsService: FaceSnapsService) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
-
-    interval(1000).pipe(
-      take(3),
-      tap(console.log)
-    ).subscribe();
   };
+
 }
