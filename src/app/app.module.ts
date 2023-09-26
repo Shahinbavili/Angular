f77ef9ed6c5b623e7808e9b61fd35d25a53b2720
shahin.bavili <shahin.bavili@useweb.com>
2023-09-26 15:06:07 +0200

    forms by reactive methode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import * as fr from '@angular/common/locales/fr';
 import {FaceSnapListComponent} from './face-snap-list/face-snap-list.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
-import {FormsModule} from "@angular/forms";
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {SingleFaceSnapComponent} from './single-face-snap/single-face-snap.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NewFaceSnapComponent} from './new-face-snap/new-face-snap.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +21,15 @@ import {FormsModule} from "@angular/forms";
     FaceSnapListComponent,
     HeaderComponent,
     LandingPageComponent,
-    SingleFaceSnapComponent
+    SingleFaceSnapComponent,
+    NewFaceSnapComponent
   ],
   imports: [
     BrowserModule,
     NgOptimizedImage,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'fr-FR'}
