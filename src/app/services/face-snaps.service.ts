5d3f8c663c9ade9006fef53bb73d9fde7255456a
shahin.bavili <shahin.bavili@useweb.com>
2023-09-26 17:42:02 +0200

    use Validators

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -79,4 +79,13 @@ export class FaceSnapsService {
     snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
   }
 
+  addFaceSnapCreatedByUser(formValue: { title: string, description: string, imageUrl: string, location?: string }) {
+    const faceSnap: FaceSnap = {
+      ...formValue,
+      createdDate: new Date(),
+      snaps: 0,
+      id: this.faceSnaps[this.faceSnaps.length - 1].id + 1
+    };
+    this.faceSnaps.push(faceSnap);
+  }
 }
