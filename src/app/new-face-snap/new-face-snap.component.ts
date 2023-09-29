6760460d040512a8829937f783c37aad840a4777
shahin.bavili <shahin.bavili@useweb.com>
2023-09-29 15:47:33 +0200

    new faceSnap by HttpClient post

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {map, Observable} from "rxjs";
+import {map, Observable, tap} from "rxjs";
 import {FaceSnap} from "../models/face-snap.model";
 import {Router} from "@angular/router";
 import {FaceSnapsService} from "../services/face-snaps.service";
@@ -24,11 +24,11 @@ export class NewFaceSnapComponent implements OnInit {
   ngOnInit(): void {
     this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
     this.snapForm = this.formBuilder.group({
-      title: [null, Validators.required],
-      description: [null, Validators.required],
-      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
-      location: [null]
-    },
+        title: [null, Validators.required],
+        description: [null, Validators.required],
+        imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+        location: [null]
+      },
       {
         updateOn: "blur"
       });
@@ -43,7 +43,8 @@ export class NewFaceSnapComponent implements OnInit {
   }
 
   onSubmitForm() {
-    this.faceSnapsService.addFaceSnapCreatedByUser(this.snapForm.value);
-    this.router.navigateByUrl('/facesnaps')
+    this.faceSnapsService.addFaceSnapCreatedByUser(this.snapForm.value).pipe(
+      tap(() => this.router.navigateByUrl('/facesnaps'))
+    ).subscribe();
   }
 }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -9,7 +9,8 @@ import {map, Observable, switchMap} from "rxjs";
 
 export class FaceSnapsService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
 
   faceSnaps: FaceSnap[] = [];
@@ -19,7 +20,7 @@ export class FaceSnapsService {
   }
 
   getFaceSnapById(id: number): Observable<FaceSnap> {
-    return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${id}`)
+    return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${ id }`)
   }
 
   snapFaceSnapById(id: number, snapType: 'snap' | 'un snap'): Observable<FaceSnap> {
@@ -28,17 +29,26 @@ export class FaceSnapsService {
         ...faceSnap,
         snaps: faceSnap.snaps + (snapType === 'snap' ? 1 : -1)
       })),
-      switchMap(updatedFaceSnap=> this.http.put<FaceSnap>(`http://localhost:3000/facesnaps/${id}`, updatedFaceSnap))
+      switchMap(updatedFaceSnap => this.http.put<FaceSnap>(`http://localhost:3000/facesnaps/${ id }`, updatedFaceSnap))
     );
   }
 
-  addFaceSnapCreatedByUser(formValue: { title: string, description: string, imageUrl: string, location?: string }) {
-    const faceSnap: FaceSnap = {
-      ...formValue,
-      createdDate: new Date(),
-      snaps: 0,
-      id: this.faceSnaps[this.faceSnaps.length - 1].id + 1
-    };
-    this.faceSnaps.push(faceSnap);
+  addFaceSnapCreatedByUser(formValue: {
+    title: string,
+    description: string,
+    imageUrl: string,
+    location?: string
+  }): Observable<FaceSnap> {
+    return this.getAllFaceSnaps().pipe(
+      map(faceSnaps => [...faceSnaps].sort((a, b) => a.id - b.id)),
+      map(sortedFaceSnaps => sortedFaceSnaps[sortedFaceSnaps.length - 1]),
+      map(previousFaceSnap => ({
+        ...formValue,
+        snaps: 0,
+        createdDate: new Date(),
+        id: previousFaceSnap.id + 1
+      })),
+      switchMap(newFaceSnap => this.http.post<FaceSnap>('http://localhost:3000/facesnaps', newFaceSnap))
+    );
   }
 }
