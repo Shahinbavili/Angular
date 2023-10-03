42b17fd126370045672d9be278ad9e65a91ec992
shahin.bavili <shahin.bavili@useweb.com>
2023-10-03 14:14:36 +0200

    secured requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {SingleFaceSnapComponent} from './single-face-snap/single-face-snap.compo
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NewFaceSnapComponent} from './new-face-snap/new-face-snap.component';
 import {HttpClientModule} from "@angular/common/http";
+import {httpInterceptorProviders} from "./interceptors";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule
   ],
   providers: [
-    {provide: LOCALE_ID, useValue: 'fr-FR'}
+    {provide: LOCALE_ID, useValue: 'fr-FR'},
+    httpInterceptorProviders
   ],
   bootstrap: [AppComponent]
 })
