d926bf617daef28306eba2de84d6e37a55974c77
shahin.bavili <shahin.bavili@useweb.com>
2023-10-03 16:02:07 +0200

    modularisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FaceSnapComponent} from './face-snap/face-snap.component';
-import {NgOptimizedImage, registerLocaleData} from "@angular/common";
-
-import * as fr from '@angular/common/locales/fr';
+import {NgOptimizedImage} from "@angular/common";
 import {FaceSnapListComponent} from './face-snap-list/face-snap-list.component';
-import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {SingleFaceSnapComponent} from './single-face-snap/single-face-snap.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NewFaceSnapComponent} from './new-face-snap/new-face-snap.component';
 import {HttpClientModule} from "@angular/common/http";
-import {httpInterceptorProviders} from "./interceptors";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
-    HeaderComponent,
     LandingPageComponent,
     SingleFaceSnapComponent,
-    NewFaceSnapComponent
+    NewFaceSnapComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,16 +28,10 @@ import {httpInterceptorProviders} from "./interceptors";
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [
-    {provide: LOCALE_ID, useValue: 'fr-FR'},
-    httpInterceptorProviders
+    HttpClientModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    registerLocaleData(fr.default);
-  }
 }
