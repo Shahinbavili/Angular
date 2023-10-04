fde5c0246b514199af58bb08a64c58492e1e8531
shahin.bavili <shahin.bavili@useweb.com>
2023-10-04 15:43:26 +0200

    authentication

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../core/services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  constructor(private auth: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  onLogin() {
+    this.auth.login();
+    this.router.navigateByUrl('/facesnaps')
+  }
+}
